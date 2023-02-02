a6a70297463ea4ada9cbcb792e18b89e4526091c
rge-github <roger.jiang@pm.me>
2023-02-02 21:16:13 +0000

    bug-docusaurus-bad-for-deep-nesting

--- initDocs.tsx
+++ initDocs.tsx
@@ -1,12 +1,13 @@
 import { existsSync, mkdirSync } from "fs"
-import { root_main_topics } from "./src/data/"
+// import { root_main_topics } from "./src/data/"
 
 console.log("this starts before Docusaurus!")
 
-console.log(root_main_topics)
+// console.log(root_main_topics.keys)
 
-const dir = "docs/new-folder"
+const dir =
+  "docs/alongerdirnamemaycauseproblems/alongerdirnamemaycauseproblems/alongerdirnamemaycauseproblems/alongerdirnamemaycauseproblems/alongerdirnamemaycauseproblems/alongerdirnamemaycauseproblems/alongerdirnamemaycauseproblems/alongerdirnamemaycauseproblems/alongerdirnamemaycauseproblems/alongerdirnamemaycauseproblems/alongerdirnamemaycauseproblems/alongerdirnamemaycauseproblems/alongerdirnamemaycauseproblems/alongerdirnamemaycauseproblems/alongerdirnamemaycauseproblems/alongerdirnamemaycauseproblems/alongerdirnamemaycauseproblems/alongerdirnamemaycauseproblems/alongerdirnamemaycauseproblems"
 
 if (!existsSync(dir)) {
-  mkdirSync(dir)
+  mkdirSync(dir, { recursive: true })
 } else console.log("dir already exists!")
