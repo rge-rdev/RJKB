560b93a2b35188ae1f9508c600687b1f5d8a761b
rge-github <roger.jiang@pm.me>
2023-03-06 18:05:52 +0000

    fix-more-specific-mdx-img-regex

--- src/theme/MDXComponents.tsx
+++ src/theme/MDXComponents.tsx
@@ -1,8 +1,10 @@
 import React from "react"
 import MDXComponents from "@theme-original/MDXComponents"
 import { Redirect } from "@docusaurus/router"
+import Image from "@theme/IdealImage"
 
 export default {
   ...MDXComponents,
   Redirect,
+  Image,
 }
