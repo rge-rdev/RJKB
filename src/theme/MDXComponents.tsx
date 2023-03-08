111ffd67f2934210c763d71a0a8d4f9526d94351
rge-github <roger.jiang@pm.me>
2023-03-08 00:52:35 +0000

    add-react-tooltips

--- src/theme/MDXComponents.tsx
+++ src/theme/MDXComponents.tsx
@@ -2,9 +2,13 @@ import React from "react"
 import MDXComponents from "@theme-original/MDXComponents"
 import { Redirect } from "@docusaurus/router"
 import Image from "@theme/IdealImage"
+import { Tooltip } from "react-tooltip"
+import tooltip_css from "react-tooltip/dist/react-tooltip.css"
 
 export default {
   ...MDXComponents,
   Redirect,
   Image,
+  Tooltip,
+  tooltip_css,
 }
