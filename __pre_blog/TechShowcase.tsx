3e7fc163cb8629f8b4d3dbfe6891c3003353500b
rge-rdev <rge.rdev@gmail.com>
2023-05-11 22:43:31 +0100

    rjkb-110520232243-f-fix-font-url

---
+++ __pre_blog/TechShowcase.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+const { FaReact, FaFly, FaNodeJs, FaSass, FaCcStripe } = React.lazy(()=>import( "react-icons/fa"))
+const {
+  SiAlgolia,
+  SiAxios,
+  SiBlender,
+  SiBootstrap,
+  SiCss3,
+  SiDocker,
+  SiEsbuild,
+  SiExpress,
+  SiFfmpeg,
+  SiFirebase,
+  SiFirefoxbrowser,
+  SiGimp,
+  SiGit,
+  SiHeroku,
+  SiHtml5,
+  SiInkscape,
+  SiJavascript,
+  SiJsonwebtokens,
+  SiMdx,
+  SiPostcss,
+  SiPostman,
+  SiReactrouter,
+  SiRedux,
+  SiStorybook,
+  SiSvg,
+  SiSvgo,
+  SiSwc,
+  SiTailwindcss,
+  SiTypescript,
+  SiUnsplash,
+  SiVite,
+  SiWebassembly,
+  SiWebpack,
+  SiYelp,
+} from "react-icons/si"
+import { TbBrandNextjs, TbBrandMongodb, TbBrandVscode } from "react-icons/tb"
+import { DocusuaurusSVG, TypesenseSVG } from "@site/static/img/svg"
+
+export const primaryProps = { size: 50 }
+export const secondaryProps = { size: 40, className: "ml-2" }
