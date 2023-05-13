6fe765c4f8eabe1f1ab049d7a9e93bf623ff32ac
rge-rdev <rge.rdev@gmail.com>
2023-05-13 13:08:41 +0100

    fix home title-70 desc-160 more descriptive

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,20 +2,17 @@
  * Copyright(c) Roger Jiang
  */
 
-import React, { lazy, Suspense } from "react"
-//@ts-ignore
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext"
+import React from "react"
 //@ts-ignore
 import Layout from "@theme/Layout"
 import HomepageFeatures from "../components/Home/HomeFeatures"
 import HomeHeader from "../components/Home/HomeHeader"
 
 export default function Home(): JSX.Element {
-  const { siteConfig } = useDocusaurusContext()
   return (
     <Layout
-      title={`${siteConfig.title}`}
-      description="COPYRIGHT ROGER JIANG 2023 RJ's Fullstack Knowledgebase & Portfolio Demo."
+      title="Search Everything FullStack in RJKB Wiki - Dictionary for Developers"
+      description="FullStack Dev Wiki with Concise Definitions for EVERY Term/Concept/Code: Browse with ⚡fast instant-search, stylish hover-preview pages, seamless SPA navigation."
     >
       <HomeHeader />
       <HomepageFeatures />
