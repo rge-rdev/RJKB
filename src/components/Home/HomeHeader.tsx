4a7797d220b546ff14929808f5290443f15c475c
rge-rdev <rge.rdev@gmail.com>
2023-05-15 19:33:46 +0100

    add Home-h1-h2 responsive reflow title-des

--- src/components/Home/HomeHeader.tsx
+++ src/components/Home/HomeHeader.tsx
@@ -2,60 +2,14 @@
  * Copyright(c) Roger Jiang
  */
 
-import React, { useEffect, useState } from "react"
+import React from "react"
 //@ts-ignore
-import Link from "@docusaurus/Link"
-//@ts-ignore
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext"
-import { IconArrowDownCircleBlackSVG } from "../../../static/img/svg"
+// import useDocusaurusContext from "@docusaurus/useDocusaurusContext"
 const HomeLogoSlider = React.lazy(() => import("./HomeLogoSlider"))
 const HomeScrollButton = React.lazy(() => import("./HomeScrollButton"))
 
 export default function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext()
-
-  const [scrollHeader, setScrollheader] = useState(false)
-  const [scrollTagline, setScrollTagline] = useState(false)
-
-  useEffect(() => {
-    const threshold = 0
-    const h_max = 64
-    const h_min = 57
-    const t_max = 375
-    const t_min = 120 //105
-
-    let lastScrollY = window.pageYOffset
-    let ticking = false
-
-    const updateScrollDir = () => {
-      const scrollY = window.pageYOffset
-
-      if (Math.abs(scrollY - lastScrollY) < threshold) {
-        ticking = false
-        return
-      }
-      lastScrollY = scrollY > 0 ? scrollY : 0
-      if (scrollY >= h_max) setScrollheader(true)
-      if (scrollY < h_max && scrollY < h_min) setScrollheader(false)
-      if (scrollY >= t_min && scrollY < t_max) {
-        setScrollTagline(true)
-      } else {
-        setScrollTagline(false)
-      }
-      ticking = false
-    }
-
-    const onScroll = () => {
-      if (!ticking) {
-        window.requestAnimationFrame(updateScrollDir)
-        ticking = true
-      }
-    }
-
-    window.addEventListener("scroll", onScroll)
-
-    return () => window.removeEventListener("scroll", onScroll)
-  }, [scrollHeader])
+  // const { siteConfig } = useDocusaurusContext()
 
   return (
     <header
@@ -66,43 +20,19 @@ export default function HomepageHeader() {
       }}
     >
       <div
-        className="flex aspect-auto flex-col items-center justify-center"
-        style={{ height: "384px", width: "100%" }}
+        className="flex aspect-auto flex-col items-center"
+        style={{ height: "415px", width: "100%" }}
       >
-        <h1
-          id="test"
-          className={`mb-5 mt-2 w-fit max-w-screen-sm rounded-full text-5xl font-extralight text-cyan-900 shadow-lg dark:text-cyan-100 ${
-            scrollHeader
-              ? "fixed top-0 z-[250] animate-fade-in-slow-75 opacity-75"
-              : "fixed top-0 animate-fade-out-fast-75 opacity-0"
-          }`}
-          style={{ height: "60px", width: "120px" }}
-        >
-          RJKB
-        </h1>
-        <h1
-          className="mb-5 mt-2 max-w-screen-sm rounded-full text-5xl text-cyan-900 shadow-lg dark:text-cyan-100"
-          style={{ height: "64px", width: "250px" }}
-        >
-          {siteConfig.title}
+        <h1 className="h-8 max-w-screen-sm rounded-br-full bg-gradient-to-r from-[#68fff0] to-[#d7ebe9] bg-clip-text pl-4 pr-8 text-3xl text-transparent shadow-lg dark:from-[#aae4ff] dark:to-[#44ffc1] sm:h-10 sm:pr-10 sm:text-4xl md:h-12 md:pr-12 md:text-5xl lg:h-[60px] lg:pr-14 lg:text-6xl xl:h-[72px] xl:max-w-3xl xl:pr-16 xl:text-7xl">
+          FullStack Wiki
         </h1>
         <h2
-          className={`mb-10 flex flex-col items-center justify-center rounded-full px-10 font-semibold italic text-cyan-200 shadow-lg dark:text-cyan-300 ${
-            scrollTagline
-              ? "fixed top-20 animate-fade-in-md-50 opacity-50"
-              : "fixed top-20 animate-fade-out-md-50 opacity-0"
-          }`}
-          style={{ height: "30px", width: "355px" }}
+          className={`mb-10 flex w-fit flex-col items-center justify-center rounded-br-full py-2 pl-10 pr-12 text-xl font-light italic text-cyan-200 shadow-lg dark:text-cyan-300 sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl`}
+          style={{
+            animation: "fadein 3s",
+          }}
         >
-          {siteConfig.tagline}
-        </h2>
-        <h2
-          className={`mb-10 flex flex-col items-center justify-center rounded-full px-10 font-light italic text-cyan-200 shadow-lg dark:text-cyan-300 ${
-            scrollTagline ? "animate-fade-out-fast-75 opacity-0" : ""
-          }`}
-          style={{ height: "30px", width: "355px" }}
-        >
-          {siteConfig.tagline}
+          Search the Best Developer Dictionary
         </h2>
         <React.Suspense
           fallback={<div style={{ width: "100%", height: "384px" }}></div>}
