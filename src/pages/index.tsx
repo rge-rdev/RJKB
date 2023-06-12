661b705962aaa892981cae092e59603b07d6049b
rge-rdev <rge.rdev@gmail.com>
2023-06-12 11:47:03 +0100

    FIX homepage-layout-suspense-flash

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,27 +3,30 @@
  */
 
 import React from "react"
-//@ts-ignore
-import Layout from "@theme/Layout"
 import HomepageFeatures from "../components/Home/HomeFeatures"
 import HomeHeader from "../components/Home/HomeHeader"
+//@ts-ignore
+// import Layout from "@theme/Layout"
+const Layout = React.lazy(() => import("@theme/Layout"))
 
 export default function Home(): JSX.Element {
   return (
-    <Layout
-      title="Search Everything FullStack in RJKB Wiki - Dictionary for Developers"
-      description="FullStack Dev Wiki with Concise Definitions for EVERY Term/Concept/Code: Browse with ⚡fast instant-search, stylish hover-preview pages, seamless SPA navigation."
-    >
-      <style>{`@keyframes fadein {
-    from {
-        opacity:0;
-    }
-    to {
-        opacity:1;
-    }
-}`}</style>
-      <HomeHeader />
-      <HomepageFeatures />
-    </Layout>
+    <React.Suspense fallback={null}>
+      <Layout
+        title="Search Everything FullStack in RJKB Wiki - Dictionary for Developers"
+        description="FullStack Dev Wiki with Concise Definitions for EVERY Term/Concept/Code: Browse with ⚡fast instant-search, stylish hover-preview pages, seamless SPA navigation."
+      >
+        <style>{`@keyframes fadein {
+        from {
+            opacity:0;
+        }
+        to {
+            opacity:1;
+        }
+    }`}</style>
+        <HomeHeader />
+        <HomepageFeatures />
+      </Layout>
+    </React.Suspense>
   )
 }
