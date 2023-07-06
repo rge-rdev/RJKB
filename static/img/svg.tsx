98537e35a68cc3fd7b889b433995e4c47531c817
rge-rdev <rge.rdev@gmail.com>
2023-07-07 00:46:47 +0100

    FIX gitignore-add-demo-static-assets

---
+++ static/img/svg.tsx
@@ -0,0 +1,120 @@
+import React from 'react'
+//@ts-ignore
+import {useColorMode} from '@docusaurus/theme-common';
+
+import IconArrowDownCircleBlackSVG from "./IconArrowDownCircleBlack.svg"
+import IconCodeEditorDiamondColorSVG from "./IconCodeEditorDiamondColor.svg"
+import IconCSS3Black from "./IconCSS3Black.svg"
+import IconDockerBlackSVG from "./IconDockerBlack.svg"
+import IconDocusaurusBlackSVG from "./IconDocusaurusBlack.svg"
+import IconDocusaurusDarkSVG from "./IconDocusaurusDark.svg"
+import IconDocusaurusLightSVG from "./IconDocusaurusLight.svg"
+import IconEsbuildColorSVG from "./IconEsbuildColor.svg"
+import IconGitBlackSVG from "./IconGitBlack.svg"
+import IconHTML5ColorSVG from "./IconHtml5Color.svg"
+import IconJavascriptBlackSVG from "./IconJavascriptBlack.svg"
+import IconNodeJSBlackSVG from "./IconNodeJsBlack.svg"
+import IconReactBlackSVG from "./IconReactBlack.svg"
+import IconReactLiveBlackSVG from "./IconReactLiveBlack.svg"
+import IconReduxOfficialBlackSVG from "./IconReduxOfficialBlack.svg"
+import IconRjnoteCodeEditorColorSVG from "./IconRjnoteCodeEditorColor.svg"
+import IconTailwindcssBlackSVG from "./IconTailwindcssBlack.svg"
+import IconTypescriptBlackSVG from "./IconTypescriptBlack.svg"
+import IconTypesenseDarkSVG from "./IconTypesenseDark.svg"
+import IconTypesenseLightSVG from "./IconTypesenseLight.svg"
+import IconVSCodeColorSVG from "./IconVSCodeColor.svg"
+import IconWebassemblyBlackSVG from "./IconWebassemblyBlack.svg"
+import IconYarnBlackSVG from "./IconYarnBlack.svg"
+import LogoDocSearchColorSVG from "../../static/img/LogoDocSearchColor.svg"
+import LogoMDXColorSVG from "./LogoMdxColor.svg"
+import LogoNgrokSVG from "./LogoNgrok.svg"
+import LogoNodeJsColorSVG from "./LogoNodeJsColor.svg"
+import LogoSwcColorDarkSVG from "./LogoSwcColorDark.svg"
+import LogoTailwindcssOfficialColorSVG from "./LogoTailwindcssOfficialColor.svg"
+import LogoTextAxiosBlackSVG from "./LogoTextAxiosBlack.svg"
+import LogoTextOnlyDockerBlackSVG from "./LogoTextOnlyDockerBlack.svg"
+import LogoTextReactRouterBlackSVG from "./LogoTextReactRouterBlack.svg"
+import LogoTextTypescriptBlackSVG from "./LogoTextTypescriptBlack.svg"
+import LogoTSNodeGreySVG from "./LogoTsNodeGrey.svg"
+import LogoTypesenseBlackSVG from "./LogoTypesenseBlack.svg"
+
+IconArrowDownCircleBlackSVG.displayName="/img/IconArrowDownCircleBlack.svg"
+IconCodeEditorDiamondColorSVG.displayName="/img/IconCodeEditorDiamondColor.svg"
+IconCSS3Black.displayName="/img/IconCSS3Black.svg"
+IconDockerBlackSVG.displayName="/img/IconDockerBlack.svg"
+IconDocusaurusBlackSVG.displayName="/img/IconDocusaurusBlack.svg"
+IconDocusaurusDarkSVG.displayName="/img/IconDocusaurusDark.svg"
+IconDocusaurusLightSVG.displayName="/img/IconDocusaurusLight.svg"
+IconEsbuildColorSVG.displayName="/img/IconEsbuildColor.svg"
+IconGitBlackSVG.displayName="/img/IconGitBlack.svg"
+IconHTML5ColorSVG.displayName="/img/IconHtml5Color.svg"
+IconJavascriptBlackSVG.displayName="/img/IconJavascriptBlack.svg"
+IconNodeJSBlackSVG.displayName="/img/IconNodeJsBlack.svg"
+IconReactBlackSVG.displayName="/img/IconReactBlack.svg"
+IconReactLiveBlackSVG.displayName="/img/IconReactLiveBlack.svg"
+IconReduxOfficialBlackSVG.displayName="/img/IconReduxOfficialBlack.svg"
+IconRjnoteCodeEditorColorSVG.displayName="/img/IconRjnoteCodeEditorColor.svg"
+IconTailwindcssBlackSVG.displayName="/img/IconTailwindcssBlack.svg"
+IconTypescriptBlackSVG.displayName="/img/IconTypescriptBlack.svg"
+IconTypesenseDarkSVG.displayName="/img/IconTypesenseDark.svg"
+IconTypesenseLightSVG.displayName="/img/IconTypesenseLight.svg"
+IconVSCodeColorSVG.displayName="/img/IconVSCodeColor.svg"
+IconWebassemblyBlackSVG.displayName="/img/IconWebassemblyBlack.svg"
+IconYarnBlackSVG.displayName="/img/IconYarnBlack.svg"
+LogoDocSearchColorSVG.displayName="/img/LogoDocSearchColor.svg"
+LogoMDXColorSVG.displayName="/img/LogoMdxColor.svg"
+LogoNgrokSVG.displayName="/img/LogoNgrok.svg"
+LogoNodeJsColorSVG.displayName="/img/LogoNodeJsColor.svg"
+LogoSwcColorDarkSVG.displayName="/img/LogoSwcColorDark.svg"
+LogoTailwindcssOfficialColorSVG.displayName="/img/LogoTailwindcssOfficialColor.svg"
+LogoTextAxiosBlackSVG.displayName="/img/LogoTextAxiosBlack.svg"
+LogoTextOnlyDockerBlackSVG.displayName="/img/LogoTextOnlyDockerBlack.svg"
+LogoTextReactRouterBlackSVG.displayName="/img/LogoTextReactRouterBlack.svg"
+LogoTextTypescriptBlackSVG.displayName="/img/LogoTextTypescriptBlack.svg"
+LogoTSNodeGreySVG.displayName="/img/LogoTsNodeGrey.svg"
+LogoTypesenseBlackSVG.displayName="/img/LogoTypesenseBlack.svg"
+
+export {
+  IconArrowDownCircleBlackSVG,
+  IconCodeEditorDiamondColorSVG,
+  IconCSS3Black,
+  IconDockerBlackSVG,
+  IconDocusaurusBlackSVG,
+  IconEsbuildColorSVG,
+  IconGitBlackSVG,
+  IconHTML5ColorSVG,
+  IconJavascriptBlackSVG,
+  IconNodeJSBlackSVG,
+  IconReactBlackSVG,
+  IconRjnoteCodeEditorColorSVG,
+  IconTailwindcssBlackSVG,
+  IconTypescriptBlackSVG,
+  IconVSCodeColorSVG,
+  IconWebassemblyBlackSVG,
+  IconYarnBlackSVG,
+  LogoMDXColorSVG,
+  IconReactLiveBlackSVG,
+  IconReduxOfficialBlackSVG,
+  LogoDocSearchColorSVG,
+  LogoNgrokSVG,
+  LogoNodeJsColorSVG,
+  LogoTailwindcssOfficialColorSVG,
+  LogoTSNodeGreySVG,
+  LogoSwcColorDarkSVG,
+  LogoTextAxiosBlackSVG,
+  LogoTextOnlyDockerBlackSVG,
+  LogoTextReactRouterBlackSVG,
+  LogoTextTypescriptBlackSVG,
+  LogoTypesenseBlackSVG,
+}
+
+export function DocusuaurusSVG(props: any){
+  const { isDarkTheme } = useColorMode();
+
+  return isDarkTheme ? <IconDocusaurusDarkSVG {...props} /> : <IconDocusaurusLightSVG  {...props}/>
+}
+export function TypesenseSVG(props: any){
+  const { isDarkTheme } = useColorMode();
+
+  return isDarkTheme ? <IconTypesenseDarkSVG {...props} /> : <IconTypesenseLightSVG  {...props}/>
+}
